0fb37848b0be2328312b69bd3292dff7c601e024
wagih <wagih.belg@gmail.com>
2022-01-21 12:06:36 +0100

    Feature Project structure & routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { UserCardComponent } from './user-card/user-card.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UserPageComponent } from './user-page/user-page.component';
+
+
+const routes: Routes = [
+  {path : '',component: HomeComponent},
+  {path : 'usersPage',component: UserPageComponent},
+  {path : 'addUser',component: UserFormComponent},
+  {path : 'test',component: UserCardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
